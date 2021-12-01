4c0a2ebcd917f4c5d46de48d643cc2e18d84ac06
ArfaGhaffar <91720371+ArfaGhaffar@users.noreply.github.com>
2021-12-02 01:14:00 +0500

    final edit

--- app/src/main/java/com/example/audioplayer/MainActivity.java
+++ app/src/main/java/com/example/audioplayer/MainActivity.java
@@ -5,15 +5,46 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     MediaPlayer player;
+    ImageView img1,img2,img3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img1=findViewById(R.id.imageView5);
+        img2=findViewById(R.id.imageView3);
+        img3=findViewById(R.id.imageView4);
+
+
+        img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             stop(view);
+            }
+        });
+
+        img2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            play(view);
+            }
+        });
+
+        img3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            pause(view);
+            }
+        });
+
+
     }
+
     public void play(View v) {
         if (player == null) {
             player = MediaPlayer.create(this, R.raw.music);
